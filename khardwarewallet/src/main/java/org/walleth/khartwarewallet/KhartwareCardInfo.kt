6ae9299ec40930a2863ae14d3524c3fafd5fb86b
ligi <ligi@ligi.de>
2020-03-07 21:17:45 +0100

    Prepare adding another module by renaming khardwarewallet -> keycard

--- khardwarewallet/src/main/java/org/walleth/khartwarewallet/KhartwareCardInfo.kt
+++
@@ -1,9 +0,0 @@
-package org.walleth.khartwarewallet
-
-data class KhartwareCardInfo(
-    val instanceUID: ByteArray,
-    val pubKey : ByteArray,
-    val version : String,
-    val remainingPairingSlots: Byte,
-    val keyUID: ByteArray
-)
